d36a5a20461cca5d799c1b32f993e10325eb46b3
Nathan Bell <nathan.bell@berkadia.com>
2019-04-26 16:24:06 -0600

    Automatically detect address we're connecting to and use it for the socket address

--- bally-front/src/api/socketConnections.js
+++ bally-front/src/api/socketConnections.js
@@ -1,5 +1,6 @@
 import openSocket from "socket.io-client";
-const socket = openSocket("http://localhost:4000");
+const address = `${window.location.protocol}//${window.location.hostname}:4000`
+const socket = openSocket(address)
 
 
 function subscribeToThing(thing, cb){
